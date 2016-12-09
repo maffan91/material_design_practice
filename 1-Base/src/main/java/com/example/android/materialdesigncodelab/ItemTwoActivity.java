818d5c8f394d8e9094d77c81b8185e91f90c8d41
Muhammad Affan Khan <maffan@folio3pk.com>
2016-12-09 18:15:14 +0500

    commit

---
+++ 1-Base/src/main/java/com/example/android/materialdesigncodelab/ItemTwoActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.materialdesigncodelab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_two);
+    }
+}
